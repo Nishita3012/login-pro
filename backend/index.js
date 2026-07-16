66381690fbbf12dbffaa979bb69aae2a1ce42e36
Nishita Panchal <nishitapanchal3012@gmail.com>
2026-07-17 00:51:53 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,38 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./db/connectDB.js";
 import { authRoutes } from "./routes/auth.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-
 app.use(express.json()); //allows us to parse incoming request:req.body
 app.use(cookieParser()); //allows us to parse incoming cookies
 
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+}
+
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
