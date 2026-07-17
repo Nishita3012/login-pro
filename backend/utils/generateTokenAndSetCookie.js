8dfede3a37f177635b5b2020006afcb14ed441f2
Nishita Panchal <nishitapanchal3012@gmail.com>
2026-07-17 15:12:12 +0530

    Fix auth cookie for cross origin deployment

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -1,17 +1,20 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
-        expiresIn: "7d",
-    })
+    const token = jwt.sign(
+        { userId },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true, // production HTTPS
+        sameSite: "none", // allow Vercel -> Render cookie
         maxAge: 7 * 24 * 60 * 60 * 1000,
-
     });
 
-    return token
-}
+    return token;
+};
