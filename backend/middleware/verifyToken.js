903617672c519969a24e7a25a64af10415697b18
Nishita Panchal <nishitapanchal3012@gmail.com>
2026-07-17 15:05:29 +0530

    Add cookie token debugging in verifyToken middleware

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+
+    console.log("Cookies:", req.cookies);
+    console.log("Token:", req.cookies.token);
+
     const token = req.cookies.token;
     if(!token) return res.status(401).json({success: false, message: "Unauthorized - no token provideed"})
     try{
